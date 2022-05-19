a34a1dd7884c96a44bb7e1414339e38f2a11fbcb
Vannara Thong <36809428+vannara43@users.noreply.github.com>
2022-05-19 05:30:37 -0700

    Add files via upload

---
+++ DotNet/NewsletterSubscriptionsAddRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests
+{
+    public class NewsletterSubscriptionsAddRequest
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(255)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
